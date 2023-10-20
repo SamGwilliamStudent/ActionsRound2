32965610a00842e6f584af99c7085d8ab9bb2446
SamGwilliamStudent <gwill34@byu.edu>
2023-10-20 14:13:01 -0600

    Added main functionality

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,39 @@
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Testing Dockerfile");
+    System.out.println("Welcome to the Calculator");
+    Calculator myCalculator = new Calculator();
+
+    while(true) {
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("Enter a command:");
+      String input = scanner.nextLine();
+
+      if (input.trim().equalsIgnoreCase("quit")) {
+        break;
+      }
+
+      String[] inputs = input.split(" ");
+      if(inputs[0].equalsIgnoreCase("add")){
+        Integer result = myCalculator.add(Integer.parseInt(inputs[1]), Integer.parseInt(inputs[2]));
+        System.out.printf("Result: %d\n", result);
+      } else if (inputs[0].equalsIgnoreCase("subtract")) {
+        Integer result = myCalculator.subtract(Integer.parseInt(inputs[1]), Integer.parseInt(inputs[2]));
+        System.out.printf("Result: %d\n", result);
+      } else if (inputs[0].equalsIgnoreCase("multiply")) {
+        Integer result = myCalculator.multiply(Integer.parseInt(inputs[1]), Integer.parseInt(inputs[2]));
+        System.out.printf("Result: %d\n", result);
+      } else if (inputs[0].equalsIgnoreCase("divide")) {
+        Integer result = myCalculator.divide(Integer.parseInt(inputs[1]), Integer.parseInt(inputs[2]));
+        System.out.printf("Result: %d\n", result);
+      } else if (inputs[0].equalsIgnoreCase("fibonacci")) {
+        Integer result = myCalculator.fibonacciNumberFinder(Integer.parseInt(inputs[1]));
+        System.out.printf("Result: %d\n", result);
+      } else if (inputs[0].equalsIgnoreCase("binary")) {
+        String result = myCalculator.intToBinaryNumber(Integer.parseInt(inputs[1]));
+        System.out.printf("Result: %s\n", result);
+      }
+    }
   }
 }
